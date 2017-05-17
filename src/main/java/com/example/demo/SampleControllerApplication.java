f603d2d7bb31b703fe952361d48aae77eaf32272
Vladislav.Borisov <Vladislav.Borisov@LTBPSOF162.bpsf.bulpros.com>
2017-05-17 14:42:59 +0300

    @RestController

--- src/main/java/com/example/demo/SampleControllerApplication.java
+++ src/main/java/com/example/demo/SampleControllerApplication.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleControllerApplication {
 
